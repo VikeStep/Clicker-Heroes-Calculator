952e9a9dff64812d2677215bc9970924c8b3ef36
VikeStep <vikestepftb@gmail.com>
2014-09-15 22:38:58 +1000

    Added Achievement Stuff. Removed Incremental

--- ClickerHeroesCalculator.js
+++ ClickerHeroesCalculator.js
@@ -1,28 +1,34 @@
 //This was made by VikeStep and stennett
 
 //Initialising Global Variables
-var saveData = ""; 												//This savedata will be in the same format as the export from Clicker Heroes itself
-var parsedSaveData = Array();								//Associative Array of the essential data from the imported save data
-var incrementalScore = 0;										//Score for Incremental
+var savedata = ""; 												//This savedata will be in the same format as the export from Clicker Heroes itself
+var parsedsavedata = Array();								//Associative Array of the essential data from the imported save data
+
 var herodata = Array();										//The data for all heroes will be an array of associated arrays (so herodata[0]["name"] will return "Cid, the Helpful Adventurer")
-var namearray = ["Cid, the Helpful Adventurer", "Treebeast", "Ivan, the Drunken Brawler", "Brittany, Beach Princess", "The Wandering Fisherman", "Betty Clicker", "The Masked Samurai", "Leon", "The Great Forest Seer", "Alexa, Assassin", "Natalia, Ice Apprentice", "Mercedes, Duchess of Blades", "Bobby, Bounty Hunter", "Broyle Lindeoven, Fire Mage", "Sir George II, King's Guard", "King Midas", "Referi Jerator, Ice Wizard", "Abbadon", "Ma Zhu", "Amenhotep", "Beastlord", "Athena, Goddess of War", "Aphrodite, Goddess of Love", "Shinatobe, Wind Deity", "Grant, the General", "Frostleaf"]; 					//List of Hero Names
-var basecostarray = [5e0,5e1,2.5e2,1e3,4e3,2e4,1e5,4e5,2.5e6,1.5e7,1e8,8e8,6.5e9,5e10,4.5e11,4e12,3.6e13,3.2e14,2.7e15,2.4e16,3e17,9e18,3.5e20,1.4e22,4.2e24,2.1e27]; //Base Cost for each Hero
+var heronames = ["Cid, the Helpful Adventurer", "Treebeast", "Ivan, the Drunken Brawler", "Brittany, Beach Princess", "The Wandering Fisherman", "Betty Clicker", "The Masked Samurai", "Leon", "The Great Forest Seer", "Alexa, Assassin", "Natalia, Ice Apprentice", "Mercedes, Duchess of Blades", "Bobby, Bounty Hunter", "Broyle Lindeoven, Fire Mage", "Sir George II, King's Guard", "King Midas", "Referi Jerator, Ice Wizard", "Abbadon", "Ma Zhu", "Amenhotep", "Beastlord", "Athena, Goddess of War", "Aphrodite, Goddess of Love", "Shinatobe, Wind Deity", "Grant, the General", "Frostleaf"]; //List of Hero Names
+var basecosts = [5e0,5e1,2.5e2,1e3,4e3,2e4,1e5,4e5,2.5e6,1.5e7,1e8,8e8,6.5e9,5e10,4.5e11,4e12,3.6e13,3.2e14,2.7e15,2.4e16,3e17,9e18,3.5e20,1.4e22,4.2e24,2.1e27]; //Base Cost for each Hero
+	
+var achievementdata = Array();								//The data for all achievements in an associated array
+var achievementnames = ["Frugal", "Stingy", "Miserly", "Greedy", "Zone Explorer", "Zone Warrior", "Zone Master", "Zone Lord", "Zone King", "Zone God", "Zone Owner", "Lethal", "Ruinous", "Calamitious", "Cataclysmic", "A lot of Damage", "Frantic", "Frenetic", "Frenzied", "Convulsions", "Deadly", "Assassin", "Restarter", "Neverending", "Again and Again", "Is this real life?", "Master of Reincarnations", "Boss Slaughter", "Boss Massacre", "Boss Exterminator", "Boss Murderer", "Boss Genocide", "Proficient Clicking", "Sore Finger", "Carpal Tunnel", "Broken Mouse", "Uptown", "Fat Cat", "Loaded", "The 1%", "The 0.01%", "Buffett", "Gates", "Rockefeller", "Guide", "Coach", "Teacher", "Mentor", "Levelupper", "Super Levelupper", "Killer", "Butcher", "Executioner", "Monster Genocide", "Terminator", "Considerate", "Generous", "Benevolent", "Magnanimous"]; //List of Achievement Names
+var achievementtypes = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; //0 - Adds DPS, 1 - Click Damage, 2 - Starting Click Damage
+var achievementrewards = [1, 2, 3, 5, 1, 2, 3, 5, 5, 5, 5, 1, 2, 3, 5, 5, 1, 3, 3, 5, 3, 5, 10, 50, 250, 1000, 2500, 1, 2, 3, 5, 5, 1, 2, 3, 5, 1, 2, 3, 5, 5, 5, 5, 5, 1, 2, 3, 5, 5, 5, 1, 2, 3, 5, 5, 1, 2, 3, 5]; //Rewards for Achievement
+
+var totalDPS = 0;
 
 //Functions for PreInit Phase - Loading all the data from local storage and parsing it
 function loadLocalStorage() {								//Will grab the data from local storage
 	if (typeof(Storage)!="undefined") {
-		saveData = localStorage.getItem("saveData");
-		incrementalScore = localStorage.getItem("incrementalScore");
+		savedata = localStorage.getItem("savedata");
 	}
 }
 
 function parseSave(data) {									//Will convert the savedata into something more readable
-	//parsedSaveData = 
+	//parsedsavedata = 
 }
 
 function preInit() {												//PreInit Phase
 	loadLocalStorage();
-	//parseSave(saveData);
+	//parseSave(savedata);
 }
 
 preInit();
@@ -32,18 +38,30 @@ function calculateBaseDPS(cost, id) {						//The base DPS for a hero is calculat
 	return Math.ceil((cost/10)*Math.pow(1-(0.0188*Math.min(id,14)),id));
 }
 
+function calculateAll() {										//Will be called initially to calculate everything and whenever
+	
+}
+
 function init() {													//Init Phase
 	for (i=0; i < 26; i++) {										//Fills herodata with known values
 		herodata[i] = Array();
-		herodata[i]["name"] = namearray[i];
-		herodata[i]["baseCost"] = basecostarray[i];
+		herodata[i]["name"] = heronames[i];
+		herodata[i]["basecost"] = basecosts[i];
 		if (i != 0) {
-			herodata[i]["baseDPS"] = calculateBaseDPS(basecostarray[i], i+1)
+			herodata[i]["baseDPS"] = calculateBaseDPS(basecosts[i], i+1);
 		} else {
-			herodata[i]["baseDPS"] = 0
+			herodata[i]["baseDPS"] = 0;
 		}
 	}
 	console.log(herodata); 											//For Debugging purposes
+	for (i=0; i < 59; i++) {
+		achievementdata[i] = Array();
+		achievementdata[i]["name"] = achievementnames[i];
+		achievementdata[i]["type"] = achievementtypes[i];
+		achievementdata[i]["reward"] = achievementrewards[i];
+		achievementdata[i]["owned"] = false; 				//Change this to actual value of imported save data when data parser is made
+	}
+	console.log(achievementdata);								//For Debugging purposes
 }
 
 init();
@@ -58,19 +76,20 @@ function numberWithCommas(number) {					//Converts 1234567 into 1,234,567. Also 
 function formatNumber(num) {								//Converts a number into what is shown InGame
 	var sign = num && num/Math.abs(num);
 	var number = Math.abs(num);
-	var SISymbols = ["","","K","M","B","T","q","Q","s","S","O","N","d","D","!","@","#","$","%","^","%","*"];
-	var digitCount = 	number && Math.floor(1+(Math.log(number)/Math.LN10));
-	if (digitCount > 64) {
-		var symbol = "*";
-		var digitsShown = digitCount - 59
-	} else if (digitCount < 6) {
-		symbol = "";
-		digitsShown = digitCount
+	var SIunits = ["","","K","M","B","T","q","Q","s","S","O","N","d","D","!","@","#","$","%","^","%","*"];
+	var digitcount = 	number && Math.floor(1+(Math.log(number)/Math.LN10));
+	var digitsshown = 0;
+	var symbol = "";
+	if (digitcount > 64) {
+		symbol = "*";
+		digitsshown = digitcount - 59
+	} else if (digitcount < 6) {
+		digitsshown = digitcount
 	} else {
-		var symbol = SISymbols[Math.floor(digitCount/3)];
-		var digitsShown = 3 + (digitCount % 3);
+		symbol = SIunits[Math.floor(digitcount/3)];
+		digitsshown = 3 + (digitcount % 3);
 	}
-	var truncNumber = Math.floor(number/Math.pow(10,digitCount-digitsShown));
+	var truncNumber = Math.floor(number/Math.pow(10,digitcount-digitsshown));
 	if (sign == 1) {
 		return numberWithCommas(truncNumber) + symbol;
 	} else if (sign == -1) {
@@ -86,28 +105,12 @@ function saveSaveData() {									//Will save the data to local storage
 	}
 }
 
-function saveIncrementalGame() {							//Will save the incremental game score
-	if (typeof(Storage)!="undefined") {
-		localStorage.setItem("incrementalScore", incrementalScore);
-	}
-}
-
-function incrementalGame() {								//Temporary Incremental Game Incrementer
-	document.getElementById("counter").innerHTML = parseInt(document.getElementById("counter").innerHTML) + 1;
-	incrementalScore = document.getElementById("counter").innerHTML;
-	saveIncrementalGame();
-}
-
 function updateDOM() {										//Will put calculated elements onto their respective DOM elements
-	if (incrementalScore != null) {
-		document.getElementById("counter").innerHTML = incrementalScore;
-	} else {
-		document.getElementById("counter").innerHTML = 0;
-	}
+	
 }
 
 function addEventListeners() {								//Everything that requires waiting for user input goes here
-	document.getElementById("counterbtn").onclick = incrementalGame; // End of Incremental Game Code
+	
 }
 
 function postInit() {											//PostInit Phase
